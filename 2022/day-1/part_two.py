49b3b63026ac93cf14c55be19d6ca1d0bfeeb698
Nathan Lucas <bnlucas@outlook.com>
2022-12-01 18:49:41 -0500

    Create part_two.py

---
+++ 2022/day-1/part_two.py
@@ -0,0 +1,21 @@
+from typing import Iterator
+
+
+def load_input() -> Iterator[Iterator[int]]:
+  input = open("input.txt").read()
+  groups = input.split("\n\n")
+
+  return map(lambda group: map(int, group.split("\n")), groups)
+
+
+def run():
+  calorie_groups = load_input()
+  
+  elves_calories = list(map(sum, calorie_groups))
+  elves_calories = sorted(elves_calories, reverse=True)
+
+  print(sum(elves_calories[:3]))
+
+
+if __name__ == '__main__':
+  run()
